205943f5d9322828e8786eb594b58b8d11f74627
rishabh choudhary <76998509+rishabhchoudhary2496@users.noreply.github.com>
2021-01-19 18:36:55 +0530

    Create dataMock.js

---
+++ ejs/dataMock.js
@@ -0,0 +1,16 @@
+const user = {
+    name:'Rishabh'
+}
+
+const data = [
+  { id: 1, title: 'A Beginner Guide to Docker', author: 'John Doe' },
+  { id: 2, title: 'A Beginner Guide to Next Js', author: 'John Doe' },
+  { id: 3, title: 'A Beginner Guide to Blockchain', author: 'John Doe' },
+  { id: 4, title: 'A Beginner Guide to Node Js', author: 'John Doe' },
+  { id: 5, title: 'A Beginner Guide to Authentication', author: 'John Doe' },
+  { id: 6, title: 'A Beginner Guide to Linux', author: 'John Doe' },
+  { id: 7, title: 'A Beginner Guide to Powershell', author: 'John Doe' },
+]
+
+module.exports = user
+module.exports = data
