909470cb0d4b84158fafa0a01e38495410531b3e
rishabh choudhary <76998509+rishabhchoudhary2496@users.noreply.github.com>
2021-02-15 18:36:51 +0530

    Update index.js

--- mongoNode/index.js
+++ mongoNode/index.js
@@ -6,6 +6,7 @@ const Article = require('./models/Article')
 const Owner = require('./models/Owner')
 const LogMessage = require('./models/Log')
 const Comment = require('./models/Comment')
+const Animal = require('./models/Animal')
 
 const app = express()
 
@@ -13,9 +14,15 @@ app.use(express.json())
 
 app.post('/user', async (req, res) => {
   console.log(req.body)
-  const { name, email, password, age } = req.body
+  const { name, email, password, age, country, state, city } = req.body
   console.log(`name ${name} email ${email} password ${password} age ${age}`)
-  let user = new User({ name, email, password, age })
+  let user = new User({
+    name,
+    email,
+    password,
+    age,
+    address: { country, state, city },
+  })
   user = await user.save()
   res.json({ user: user })
 })
@@ -314,33 +321,157 @@ app.post('/comment', async (req, res) => {
   res.json({ comment: comment })
 })
 
+// app.get('/comment', async (req, res) => {
+//   const comments = await Comment.aggregate([
+//     {
+//       $lookup: {
+//         from: 'users',
+//         localField: 'user',
+//         foreignField: '_id',
+//         as: 'User',
+//       },
+//     },
+//     {
+//       $unwind: '$User',
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         __v: 0,
+//         user: 0,
+//         'User._id': 0,
+//         'User.password': 0,
+//         'User.__v': 0,
+//       },
+//     },
+//   ]) //using lookup aggregation to join user to comment
+//   //using unwind with to replace userArray with user
+//   //hiding fields using project
+//   res.json({ comments: comments })
+// })
+
+//========================learning indexes=====================
+app.post('/animal', async (req, res) => {
+  const { name, type, tags } = req.body
+  const animal = await Animal.create({ name, type, tags })
+  res.json({ animal })
+})
+
+app.get('/animal', async (req, res) => {
+  const animal = await Animal.find()
+  res.json({ animal })
+})
+
+//=========================mongoose using mondo db with node=============
+// static model methods
+
+app.get('/animalByType', async (req, res) => {
+  const animals = await Animal.findByType('panther')
+  res.json(animals)
+})
+
+//using query methods
+app.get('/animalByName', async (req, res) => {
+  const animal = await Animal.findOne({ name: 'bageera' })
+  console.log(animal.namePlusType)
+  res.json(animal)
+})
+
+//testing if validators run on update document
+// app.put('/user/:id', async (req, res) => {
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.params.id },
+//     {
+//       $set: {
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: req.body.password,
+//         age: req.body.age,
+//       },
+//     },
+//     { new: true, runValidators: true }
+//   )
+//   res.json({ user })
+// })
+
+//using select to select which fields to show
+//using populate to populate reference document
+
+//find
 app.get('/comment', async (req, res) => {
-  const comments = await Comment.aggregate([
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'user',
-        foreignField: '_id',
-        as: 'User',
-      },
-    },
-    {
-      $unwind: '$User',
-    },
-    {
-      $project: {
-        _id: 0,
-        __v: 0,
-        user: 0,
-        'User._id': 0,
-        'User.password': 0,
-        'User.__v': 0,
-      },
-    },
-  ]) //using lookup aggregation to join user to comment
-  //using unwind with to replace userArray with user
-  //hiding fields using project
-  res.json({ comments: comments })
+  const comments = await Comment.find()
+    .populate({ path: 'user', select: '-_id  -__v' })
+    .select('-_id -__v ')
+  res.json({ comments })
+})
+
+// findOne and Replace
+app.put('/animal/:id', async (req, res) => {
+  const animal = await Animal.findOneAndReplace(
+    { _id: req.params.id },
+    { name: req.body.name, type: req.body.type, tags: req.body.tags },
+    { new: true }
+  )
+
+  res.json(animal)
+})
+
+//findOneAndUpdate
+app.put('/user/:id', async (req, res) => {
+  const user = await User.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: { name: req.body.name, age: req.body.age } },
+    { new: true }
+  )
+  res.json({ user: user })
+})
+
+// mongoose aggregations
+
+//addFields add new Fields
+app.get('/userAggr', async (req, res) => {
+  let users = await User.aggregate()
+    .match({ age: { $gte: 20 } })
+    .addFields({
+      halfAge: { $divide: ['$age', 2] },
+    })
+    .project({ _id: 0, __v: 0 })
+    .sort({ age: -1 })
+    .skip(1)
+
+  res.json({ users })
+})
+
+//unwind aggregation stage with mongoose
+app.get('/ownerAggr', async (req, res) => {
+  const owner = await Owner.aggregate().unwind('pets')
+  res.json(owner)
+})
+
+//lookup stage with mongoose
+app.get('/commentAggr', async (req, res) => {
+  const comment = await Comment.aggregate()
+    .lookup({
+      from: 'users',
+      localField: 'user',
+      foreignField: '_id',
+      as: 'user',
+    })
+    .unwind('user')
+    .project({
+      _id: 0,
+      __v: 0,
+      'user._id': 0,
+      'user.__v': 0,
+      'user.password': 0,
+    })
+  // .sample(1)//randomly pick from doc
+  res.json(comment)
+})
+
+app.delete('/user/:id', async (req, res) => {
+  const user = await User.findOneAndDelete({ _id: req.params.id })
+  console.log(user)
 })
 
 const PORT = process.env.PORT || 5000
@@ -351,3 +482,4 @@ app.listen(PORT, 'localhost', () => {
 //
 
 // db.createView('sampleView', 'users', [{ $project: { _id: 0, password: 0 } }])
+
